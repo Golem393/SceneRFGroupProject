5efff2c579aa23cddf7e8df62937e42a3289680e
umer.abbasi <umer.abbasi@tum.de>
2025-01-19 02:33:40 +0000

    script to download the zip files

---
+++ download_bundlefusion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download .zip files of 8 scenes
+wget https://graphics.stanford.edu/projects/bundlefusion/data/apt0/apt0.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/apt1/apt1.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/apt2/apt2.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/copyroom/copyroom.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/office0/office0.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/office0/office1.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/office0/office2.zip
+wget https://graphics.stanford.edu/projects/bundlefusion/data/office0/office3.zip
