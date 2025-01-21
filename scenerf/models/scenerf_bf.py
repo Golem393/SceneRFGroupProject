6e4d5d6b2e91565ce9ea869b054579e9f7acf53c
Flavio Arrigoni <f.arrigoni@student.tugraz.at>
2025-01-21 08:31:00 +0100

    None fix debugging

--- scenerf/models/scenerf_bf.py
+++ scenerf/models/scenerf_bf.py
@@ -708,6 +708,7 @@ class SceneRF(pl.LightningModule):
                 colors=colors)
             if hierarchical_sampling and iteration == 0:
                 weights_temp = rendered_out['weights']
+        print(rendered_out)
 
 
         depths = rendered_out['depth_rendered']
