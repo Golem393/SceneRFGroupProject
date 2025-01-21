4ec98a4e53400dadf35fcd7ec064f198b5a16df4
Flavio Arrigoni <f.arrigoni@student.tugraz.at>
2025-01-21 08:51:37 +0100

    error debugging

--- scenerf/models/scenerf_bf.py
+++ scenerf/models/scenerf_bf.py
@@ -630,7 +630,7 @@ class SceneRF(pl.LightningModule):
             self.img_size,
             sampling_method="uniform",
             sampled_pixels=batch_sampled_pixels,
-            n_pts_per_ray=n_pts_uni // 2,
+            n_pts_per_ray=n_pts_uni,
             max_sample_depth=self.max_sample_depth)
 
         gaussian_means_sensor_distance, gaussian_stds_sensor_distance = self.predict_gaussian_means_and_stds(
@@ -663,7 +663,7 @@ class SceneRF(pl.LightningModule):
                     self.img_size,
                     sampling_method="uniform",
                     sampled_pixels=batch_sampled_pixels,
-                    n_pts_per_ray=n_pts_uni // 2,
+                    n_pts_per_ray=n_pts_uni,
                     max_sample_depth=self.max_sample_depth,
                     weights=weights_temp)
                 cam_pts = torch.cat([cam_pts_uni, cam_pts_gauss, cam_pts_hier],
@@ -688,8 +688,6 @@ class SceneRF(pl.LightningModule):
                     cam_pts = cam_pts_gauss
                     depth_volume = depth_volume_gauss
                     sensor_distance = sensor_distance_gauss
-
-            print(cam_pts)
 
             sorted_indices = torch.argsort(sensor_distance, dim=1)
             sensor_distance = torch.gather(
