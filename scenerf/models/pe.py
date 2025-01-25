9a7ba8b08f822d3d09532348e11cad26d70c84be
Ahmed El Ghobashy <ahmadsamir694@gmail.com>
2025-01-25 13:24:26 +0100

    add logging for debug

--- scenerf/models/pe.py
+++ scenerf/models/pe.py
@@ -12,8 +12,7 @@ class RFFEncoding(torch.nn.Module):
     def __init__(self, num_freqs=6, d_in=3, sigma=1.0, include_input=True):
         super().__init__()
         # instead of D we have 2D 
-        num_freqs = 2 * num_freqs
-        self.num_freqs = num_freqs
+        self.num_freqs = 2 *num_freqs
         self.d_in = d_in
         self.include_input = include_input
         # Output dimensions:  (cos) per frequency for each input dimension
@@ -24,9 +23,10 @@ class RFFEncoding(torch.nn.Module):
         # Randomly sample frequencies w' ~ N(0, sigma^2)
         freqs = 2 * np.pi * torch.randn(d_in, self.num_freqs) * sigma
         self.register_buffer("_freqs", freqs)
+        print("freqs shape is ",freqs.shape)
 
         # Randomly sample biases b' ~ U[0, 2pi]
-        biases = 2 * np.pi * torch.rand(num_freqs)
+        biases = 2 * np.pi * torch.rand(self.num_freqs)
         self.register_buffer("_biases", biases)
 
         
@@ -38,7 +38,9 @@ class RFFEncoding(torch.nn.Module):
         :return: Encoded tensor of shape (batch_size, d_out)
         """
         # Compute the projection: 2 * pi * (x @ freqs) + biases
+        print("x shape is ", x.shape)
         projected = torch.matmul(x, self._freqs) + self._biases
+        print("projected shape is ", projected.shape)
 
         # Compute  cosine embeddings
         cos_enc = torch.sqrt(torch.tensor(2.0)) * torch.cos(projected)
